56d62e54a57935efe0aca534265dec5b276671c1
Sum7011it <86011830+Sum7011it@users.noreply.github.com>
2024-01-03 12:19:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,5 +58,103 @@ fetch(json_url).then(Response => Response.json())
             search.appendChild(card);
         });
         //serach filter
-        
+        search_input.addEventListener('keyup',()=>{
+            let filter = search_input.value.toUpperCase();
+            let a = search.getElementsByTagName('a');
+
+            for(let index =0;index < a.length;index++){
+                let b= a[index].getElementsByClassName('cont')[0];
+                let TextValue = b.textContent || b.innerText
+                if(TextValue.toUpperCase().indexOf(filter) > -1){
+                    a[index].style.display = "flex";
+                    search.style.visibility = "visible";
+                    search.style.opacity = 1;
+                }else{
+                    a[index].style.display = "none";
+                    search.style.visibility = "visible";
+                    search.style.opacity = 1;
+                }
+                if(search_input.value == 0){
+                    search.style.visibility = "hidden";
+                    search.style.opacity = 0;
+                }
+            }
+        })
+        let video = document.getElementsByTagName('video')[0];
+        let play =document.getElementById('play');
+
+        play.addEventListener('click',()=>{
+            if(video.paused){
+                video.play();
+                play.innerHTML = `play <i class="bi bi-pause-fill"></i>`
+            }else{
+                play.innerHTML = `Watch <i class="bi bi-play-fill"></i>`
+                video.pause();
+            }
+        })
+
+        let series = document.getElementById('series');
+        let movies = document.getElementById('movies');
+
+        series.addEventListener('click',()=>{
+            cards.innerHTML = '';
+
+            let series_array = data.filter(ele => {
+                return ele.type ==="series";
+            })
+
+            series_array.forEach((ele, i) => {
+                let { name, imdb, date, sposter, bposter, genre, url } = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}{</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill">${imdb}</i></h3>
+                        </div>
+                    </div>
+                </div>
+                `
+                cards.appendChild(card);
+            });
+            
+
+        })
+        movies.addEventListener('click',()=>{
+            cards.innerHTML = '';
+
+            let movie_array = data.filter(ele => {
+                return ele.type ==="movie";
+            })
+
+            movie_array.forEach((ele, i) => {
+                let { name, imdb, date, sposter, bposter, genre, url } = ele;
+                let card = document.createElement('a');
+                card.classList.add('card');
+                card.href = url;
+                card.innerHTML = `
+                <img src="${sposter}" alt="${name}" class="poster">
+                <div class="rest_card">
+                    <img src="${bposter}" alt="">
+                    <div class="cont">
+                        <h4>${name}</h4>
+                        <div class="sub">
+                            <p>${genre}, ${date}{</p>
+                            <h3><span>IMDB</span><i class="bi bi-star-fill">${imdb}</i></h3>
+                        </div>
+                    </div>
+                </div>
+                `
+                cards.appendChild(card);
+            });
+            
+
+        })
+
     });
